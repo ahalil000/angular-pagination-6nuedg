476495af2e416fce3d2650782e83dcb75f327d06
ahalil000 <andrew.halil@outlook.com>
2021-05-23 21:57:37 +1000

    3 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PaginationComponent } from './pagination.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, MatPaginatorModule ],
+  declarations: [ AppComponent, HelloComponent, PaginationComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
